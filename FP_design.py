ad248388e05bcd1ecfd3d7ea51e2d37934a519fd
bellaohr <bellaohr@college.harvard.edu>
2026-04-09 15:24:09 +0000

    Thu, Apr 9, 2026, 11:24 AM -04:00

--- FP_design.py
+++ FP_design.py
@@ -2,14 +2,13 @@
 '''
 
 def updated_blur_state(current_blur_state, answer, guess):
-    state = list(current_blur_state)
-    result = []
+    state = list(current_blur_state) #converts current_blur_state into a list that's stored in state
 
-    for i in range(len(answer)):
-        if guess[i] == answer[i]:
-            state[i] = answer[i]
+    for i in range(len(answer)): #loops thru each index of the word
+        if guess[i] == answer[i]: #checks if player guess matches word @ given index
+            state[i] = answer[i] #if they match, replace * with the letter
 
-    return updated_blur_state
+    return state
 
 
 
