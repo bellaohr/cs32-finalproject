40af39d5c6ec14d44f2c758408ef852f5a8fdbfb
bellaohr <bellaohr@college.harvard.edu>
2026-04-09 14:34:16 +0000

    Thu, Apr 9, 2026, 10:34 AM -04:00

--- FP_design.py
+++ FP_design.py
@@ -1,3 +1,6 @@
+''' because I'm doing client and server, not sure how to do this...
+'''
+
 def updated_blur_state(current_blur_state, answer, guess):
     state = list(current_blur_state)
     result = []
