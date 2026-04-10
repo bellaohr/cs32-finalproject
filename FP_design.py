c16dc2f3d87183e933a4c3d11cd53ead0529e2db
bellaohr <bellaohr@college.harvard.edu>
2026-04-10 01:09:26 +0000

    Thu, Apr 9, 2026, 9:09 PM -04:00

--- FP_design.py
+++ FP_design.py
@@ -1,14 +1,18 @@
 ''' because I'm doing client and server, not sure how to do this...
 '''
 
-def updated_blur_state(current_blur_state, answer, guess):
-    state = list(current_blur_state) #converts current_blur_state into a list that's stored in state
+def updated_blur_state(current_blur_state, secret, guess):
+    revealed = list(current_blur_state) #converts current_blur_state into a list that's stored in state
 
-    for i in range(len(answer)): #loops thru each index of the word
-        if guess[i] == answer[i]: #checks if player guess matches word @ given index
-            state[i] = answer[i] #if they match, replace * with the letter
+    for i in range(len(secret)): #loops thru each index of the word
+        if guess[i] == secret[i]: #checks if player guess matches word @ given index
+            revealed[i] = guess[i] #if they match, replace * with the letter
+            correct_spot += 1
 
-    return state
+    return revealed
+
+
+
 
 
 
